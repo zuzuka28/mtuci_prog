889f34e61d3e9c030a9540b70b411edde83f6d0f
zuzuka28 <griferdirr44@list.ru>
2021-10-28 20:20:26 +0300

    exeprions for registration on MyWebApp

--- MyWebApp/app.py
+++ MyWebApp/app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
-conn = psycopg2.connect(database="service_db", user="sanya", password="1111", host="localhost", port="5432")
+conn = psycopg2.connect(database="service_db", user="sasha", password="password", host="localhost", port="5432")
 cursor = conn.cursor()
 
 
-@app.route('/index', methods=['GET'])
+@app.route('/login/', methods=['GET'])
 def index():
     return render_template('login.html')
 
@@ -16,6 +16,12 @@ def index():
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
-    return render_template('account.html', full_name=records[0][1])
+    if username and password:
+        cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+        records = list(cursor.fetchall())
+        if records:
+            return render_template('account.html', full_name=records[0][1], log_pass=username+":"+password)
+        return "<p>THERE IS NO USER<p>"
+    return "<p>TRY TO ENTER LOGIN AND PASSWORD. BOTH OF THEM. OK?<p>"
+
+
