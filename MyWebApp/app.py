57b1c33aa9060b4138996e446dae1a679304197a
zuzuka28 <griferdirr44@list.ru>
2021-11-03 11:14:02 +0300

    add registration

--- MyWebApp/app.py
+++ MyWebApp/app.py
@@ -1,27 +1,42 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
 conn = psycopg2.connect(database="service_db", user="sasha", password="password", host="localhost", port="5432")
 cursor = conn.cursor()
 
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
 
-@app.route('/login/', methods=['GET'])
-def index():
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
     return render_template('login.html')
 
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
 
-@app.route('/login/', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if username and password:
-        cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-        records = list(cursor.fetchall())
-        if records:
-            return render_template('account.html', full_name=records[0][1], log_pass=username+":"+password)
-        return "<p>THERE IS NO USER<p>"
-    return "<p>TRY TO ENTER LOGIN AND PASSWORD. BOTH OF THEM. OK?<p>"
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
 
+        return redirect('/login/')
+
+    return render_template('registration.html')
+
+
+
+app.run()
 
